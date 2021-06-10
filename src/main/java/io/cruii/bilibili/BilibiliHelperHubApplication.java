7f2a167abfb2544010accc9e3c8bcecea8d9c260
Cruii <cruii811@gmail.com>
2021-06-10 21:02:44 +0800

    :recycle: 取消数据源配置

--- src/main/java/io/cruii/bilibili/BilibiliHelperHubApplication.java
+++ src/main/java/io/cruii/bilibili/BilibiliHelperHubApplication.java
@@ -1,11 +1,10 @@
 package io.cruii.bilibili;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@MapperScan("io.cruii.bilibili.mapper")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BilibiliHelperHubApplication {
 
     public static void main(String[] args) {
