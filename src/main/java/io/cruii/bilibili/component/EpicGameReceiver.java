cf6cbfcfc05624f0255c01e1206ad797397c5d24
Cruii <cruii811@gmail.com>
2021-08-03 14:19:55 +0800

    :sparkles: feat(epic游戏领取): 新增epic游戏领取模块

---
+++ src/main/java/io/cruii/bilibili/component/EpicGameReceiver.java
@@ -0,0 +1,11 @@
+package io.cruii.bilibili.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author cruii
+ * Created on 2021/8/3
+ */
+@Component
+public class EpicGameReceiver {
+}
