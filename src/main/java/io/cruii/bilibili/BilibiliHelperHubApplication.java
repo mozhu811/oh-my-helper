d6923ad7644ab547efb2ce5fba76fa4a57985055
Cruii <cruii811@gmail.com>
2021-09-18 10:34:40 +0800

    :wrench: refactor(主启动类): 不在排除数据源配置

--- src/main/java/io/cruii/bilibili/BilibiliHelperHubApplication.java
+++ src/main/java/io/cruii/bilibili/BilibiliHelperHubApplication.java
@@ -2,10 +2,9 @@ package io.cruii.bilibili;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableScheduling
 public class BilibiliHelperHubApplication {
 
