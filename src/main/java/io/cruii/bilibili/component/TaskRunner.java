f573af5252c9e2bad58b24e653d4043ed3eb3650
Cruii <cruii811@gmail.com>
2021-09-30 10:57:45 +0800

    :sparkles: feat(task): 在运行任务完成后更新用户信息

--- src/main/java/io/cruii/bilibili/component/TaskRunner.java
+++ src/main/java/io/cruii/bilibili/component/TaskRunner.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.text.CharSequenceUtil;
 import io.cruii.bilibili.entity.TaskConfig;
 import io.cruii.bilibili.push.QyWechatPusher;
+import io.cruii.bilibili.repository.BilibiliUserRepository;
 import io.cruii.bilibili.repository.TaskConfigRepository;
 import io.cruii.bilibili.service.BilibiliUserService;
 import lombok.extern.log4j.Log4j2;
@@ -15,6 +16,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
@@ -31,12 +33,16 @@ public class TaskRunner {
     private static final Map<String, TaskConfig> CACHE = new HashMap<>();
     protected static final BlockingQueue<String> FINISH_QUEUE = new LinkedBlockingDeque<>();
     private final TaskConfigRepository taskConfigRepository;
+    private final BilibiliUserRepository bilibiliUserRepository;
     private final Executor bilibiliExecutor;
     private final BilibiliUserService userService;
 
     public TaskRunner(TaskConfigRepository taskConfigRepository,
-                      Executor bilibiliExecutor, BilibiliUserService userService) {
+                      BilibiliUserRepository bilibiliUserRepository,
+                      Executor bilibiliExecutor,
+                      BilibiliUserService userService) {
         this.taskConfigRepository = taskConfigRepository;
+        this.bilibiliUserRepository = bilibiliUserRepository;
         this.bilibiliExecutor = bilibiliExecutor;
         this.userService = userService;
     }
@@ -46,15 +52,29 @@ public class TaskRunner {
             try {
                 while (true) {
                     String traceId = FINISH_QUEUE.take();
+                    TaskConfig taskConfig = CACHE.get(traceId);
+
                     String date = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.now());
-                    String content = FileUtil.readLines(new File("logs/all-" + date + ".log"), StandardCharsets.UTF_8)
-                            .stream().filter(line -> line.contains(traceId))
+                    List<String> logs = FileUtil.readLines(new File("logs/all-" + date + ".log"), StandardCharsets.UTF_8);
+                    String content = logs
+                            .stream()
+                            .filter(line -> line.contains(traceId))
                             .map(line -> line.split("\\|\\|")[1])
                             .collect(Collectors.joining("<br>"));
 
-                    TaskConfig taskConfig = CACHE.get(traceId);
+                    bilibiliUserRepository
+                            .findById(taskConfig.getDedeuserid())
+                            .ifPresent(user -> logs.stream()
+                                    .filter(line -> line.contains("按照当前进度"))
+                                    .map(line -> line.substring(line.lastIndexOf(":") + 1, line.length() - 1).trim())
+                                    .findFirst()
+                                    .ifPresent(upgradeDays -> {
+                                        user.setUpgradeDays(Integer.valueOf(upgradeDays));
+                                        bilibiliUserRepository.save(user);
+                                    })
+                            );
 
-                    userService.save(taskConfig.getDedeuserid(), taskConfig.getSessdata(), taskConfig.getBiliJct());
+                    userService.saveAndUpdate(taskConfig.getDedeuserid(), taskConfig.getSessdata(), taskConfig.getBiliJct());
                     String corpId = taskConfig.getCorpId();
                     String corpSecret = taskConfig.getCorpSecret();
                     String agentId = taskConfig.getAgentId();
