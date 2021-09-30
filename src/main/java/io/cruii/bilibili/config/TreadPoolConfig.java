cf3e991702f49bf14a9a7fc275ba393d23fac2f1
Cruii <cruii811@gmail.com>
2021-09-30 10:59:25 +0800

    :wrench: feat(线程池): 修改线程池大小

--- src/main/java/io/cruii/bilibili/config/TreadPoolConfig.java
+++ src/main/java/io/cruii/bilibili/config/TreadPoolConfig.java
@@ -20,9 +20,9 @@ public class TreadPoolConfig {
     public Executor bilibiliExecutor() {
         ThreadPoolTaskExecutor executor = new TaskThreadPoolExecutor();
         // 核心线程数：线程池创建时候初始化的线程数
-        executor.setCorePoolSize(10);
+        executor.setCorePoolSize(5);
         // 最大线程数：线程池最大的线程数，只有在缓冲队列满了之后才会申请超过核心线程数的线程
-        executor.setMaxPoolSize(20);
+        executor.setMaxPoolSize(10);
         // 缓冲队列：用来缓冲执行任务的队列
         executor.setQueueCapacity(500);
         // 允许线程的空闲时间60秒：当超过了核心线程之外的线程在空闲时间到达之后会被销毁
