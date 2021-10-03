cbda5365e79f00bca52a433b906832e589cf1689
Cruii <cruii811@gmail.com>
2021-10-03 23:25:16 +0800

    :loud_sound: feat(push): 新增推送日志

--- src/main/java/io/cruii/bilibili/push/TelegramBotPusher.java
+++ src/main/java/io/cruii/bilibili/push/TelegramBotPusher.java
@@ -1,13 +1,17 @@
 package io.cruii.bilibili.push;
 
 import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * @author cruii
  * Created on 2021/10/03
  */
-public class TelegramBotPusher implements Pusher{
+@Log4j2
+public class TelegramBotPusher implements Pusher {
     private final String token;
 
     private final String chatId;
@@ -22,6 +26,11 @@ public class TelegramBotPusher implements Pusher{
         String url = UriComponentsBuilder
                 .fromHttpUrl("https://api.telegram.org/bot{token}/sendMessage?chat_id={chatId}&text={content}")
                 .build(token, chatId, content).toString();
-        HttpRequest.post(url).execute();
+        JSONObject resp = JSONUtil.parseObj(HttpRequest.get(url).execute().body());
+        if (Boolean.TRUE.equals(resp.getBool("ok"))) {
+            log.info("Telegram Bot 推送成功");
+        } else {
+            log.error("Telegram Bot 推送失败：{}", resp);
+        }
     }
 }
