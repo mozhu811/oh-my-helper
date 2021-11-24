9fc76748206f7a22bff757f562ee661002b25b9c
Cruii <cruii811@gmail.com>
2021-11-24 14:25:57 +0800

    :sparkles: feat(工具类): 新增腾讯云cos工具类

---
+++ src/main/java/io/cruii/bilibili/util/CosUtil.java
@@ -0,0 +1,55 @@
+package io.cruii.bilibili.util;
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.region.Region;
+import io.cruii.bilibili.config.TencentCloudConfig;
+
+import java.io.File;
+
+/**
+ * @author cruii
+ * Created on 2021/11/23
+ */
+public class CosUtil {
+    private CosUtil() {
+    }
+
+    private static TencentCloudConfig tencentCloudConfig;
+
+    private static COSClient cosClient;
+
+    /**
+     * 初始化
+     */
+    private static void setup() {
+        tencentCloudConfig = SpringUtil.getApplicationContext().getBean("tencentCloudConfig", TencentCloudConfig.class);
+        BasicCOSCredentials credentials = new BasicCOSCredentials(tencentCloudConfig.getSecretId(), tencentCloudConfig.getSecretKey());
+        ClientConfig clientConfig = new ClientConfig(new Region(tencentCloudConfig.getCosRegion()));
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+        cosClient = new COSClient(credentials, clientConfig);
+    }
+
+    /**
+     * 上传对象
+     *
+     * @param file 待上传的文件
+     */
+    public static void upload(File file) {
+        if (cosClient == null) {
+            setup();
+        }
+
+        try {
+            String key = tencentCloudConfig.getFolder() + "/" + file.getName();
+            PutObjectRequest putObjectRequest = new PutObjectRequest(tencentCloudConfig.getBucketName(), key, file);
+            cosClient.putObject(putObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
