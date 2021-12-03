b75a1bd3b9fee4eb881732e26f8c167f3a6e0677
Cruii <cruii811@gmail.com>
2021-12-03 15:29:14 +0800

    :wrench: refactor(线程池): 调整推送线程池参数

--- src/main/java/io/cruii/bilibili/config/TreadPoolConfig.java
+++ src/main/java/io/cruii/bilibili/config/TreadPoolConfig.java
@@ -3,7 +3,6 @@ package io.cruii.bilibili.config;
 import io.cruii.bilibili.component.TaskThreadPoolExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ThreadPoolExecutor;
@@ -13,15 +12,14 @@ import java.util.concurrent.ThreadPoolExecutor;
  * Created on 2021/6/11
  */
 @Configuration
-@EnableAsync
 public class TreadPoolConfig {
     @Bean
     public ThreadPoolTaskExecutor bilibiliExecutor() {
         ThreadPoolTaskExecutor executor = new TaskThreadPoolExecutor();
         // 核心线程数：线程池创建时候初始化的线程数
-        executor.setCorePoolSize(20);
+        executor.setCorePoolSize(50);
         // 最大线程数：线程池最大的线程数，只有在缓冲队列满了之后才会申请超过核心线程数的线程
-        executor.setMaxPoolSize(20);
+        executor.setMaxPoolSize(50);
         // 缓冲队列：用来缓冲执行任务的队列
         executor.setQueueCapacity(200);
         // 允许线程的空闲时间60秒：当超过了核心线程之外的线程在空闲时间到达之后会被销毁
