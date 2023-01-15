926f2024c162420179a2e77fd77e494e8832c165
Cruii <cruii811@gmail.com>
2023-01-15 15:28:03 +0800

    :zap: 优化任务执行机制

--- hh-execution/src/main/java/io/cruii/execution/component/TaskRunner.java
+++ hh-execution/src/main/java/io/cruii/execution/component/TaskRunner.java
@@ -4,12 +4,10 @@ import cn.hutool.core.io.FileUtil;
 import io.cruii.component.BilibiliDelegate;
 import io.cruii.component.TaskExecutor;
 import io.cruii.context.BilibiliUserContext;
-import io.cruii.execution.feign.BilibiliFeignService;
 import io.cruii.execution.feign.PushFeignService;
 import io.cruii.pojo.po.BilibiliUser;
 import io.cruii.pojo.po.TaskConfig;
-import io.cruii.pojo.vo.BilibiliUserVO;
-import ma.glasnost.orika.MapperFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -20,6 +18,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 /**
@@ -27,71 +27,87 @@ import java.util.stream.Collectors;
  * Created on 2022/10/17
  */
 @Component
+@Slf4j
 public class TaskRunner {
 
     private final ThreadPoolTaskExecutor taskExecutor;
-    private final ThreadPoolTaskExecutor pushExecutor;
-    private final BilibiliFeignService bilibiliFeignService;
 
     private final PushFeignService pushFeignService;
 
-    private final MapperFactory mapperFactory;
+    private static final BlockingQueue<String> PUSH_QUEUE = new LinkedBlockingQueue<>(3000);
 
     public TaskRunner(ThreadPoolTaskExecutor taskExecutor,
-                      ThreadPoolTaskExecutor pushExecutor,
-                      BilibiliFeignService bilibiliFeignService,
-                      PushFeignService pushFeignService,
-                      MapperFactory mapperFactory) {
+                      PushFeignService pushFeignService) {
         this.taskExecutor = taskExecutor;
-        this.pushExecutor = pushExecutor;
-        this.bilibiliFeignService = bilibiliFeignService;
         this.pushFeignService = pushFeignService;
-        this.mapperFactory = mapperFactory;
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    String msg = PUSH_QUEUE.take();
+                    String dedeuserid = msg.split(":")[0];
+                    String traceId = msg.split(":")[1];
+                    // 日志收集
+                    String date = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.now());
+                    File logFile = new File("logs/execution/all-" + date + ".log");
+                    String content = null;
+                    if (logFile.exists()) {
+                        List<String> logs = FileUtil.readLines(logFile, StandardCharsets.UTF_8);
+
+                        content = logs
+                                .stream()
+                                .filter(line -> line.contains(traceId) && (line.contains("INFO") || line.contains("ERROR")))
+                                .map(line -> line.split("\\|\\|")[1])
+                                .collect(Collectors.joining("\n"));
+                    }
+                    this.pushFeignService.push(dedeuserid, content);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
     }
 
-    public void run(TaskConfig taskConfig) {
-        taskExecutor.execute(() -> {
+    public BilibiliUser run(TaskConfig taskConfig) {
+        AtomicReference<BilibiliUser> ret = new AtomicReference<>();
+        log.debug("Current task thread pool queue size: {}", taskExecutor.getThreadPoolExecutor().getQueue().size());
+        log.debug("Current task thread pool active count: {}", taskExecutor.getActiveCount());
+        log.debug("Current task thread pool size: {}", taskExecutor.getPoolSize());
+        log.debug("Current task thread pool completed task count: {}", taskExecutor.getThreadPoolExecutor().getCompletedTaskCount());
+        Future<String> future = taskExecutor.submit(() -> {
             try {
                 BilibiliDelegate delegate = new BilibiliDelegate(taskConfig);
                 BilibiliUser user = delegate.getUser();
-                BilibiliUserContext.set(user);
-                TaskExecutor executor = new TaskExecutor(delegate);
-                BilibiliUser retUser = executor.execute();
-                retUser.setLastRunTime(LocalDateTime.now());
-                BilibiliUserContext.set(retUser);
-
+                if (user != null) {
+                    user.setLastRunTime(LocalDateTime.now());
+                    BilibiliUserContext.set(user);
+                    TaskExecutor executor = new TaskExecutor(delegate);
+                    ret.set(executor.execute());
+                    BilibiliUserContext.remove();
+                }
+                return MDC.get("traceId");
             } catch (Exception e) {
-                throw new RuntimeException("执行任务发生异常", e);
+                Thread.currentThread().interrupt();
+                log.error("执行任务发生异常", e);
             } finally {
-                String traceId = MDC.get("traceId");
-                // 推送
-                push(taskConfig.getDedeuserid(), traceId);
                 MDC.clear();
             }
+            return null;
         });
-    }
 
-    private void push(String dedeuserid, String traceId) {
-        pushExecutor.execute(() -> {
-            BilibiliUser retUser = BilibiliUserContext.get();
-            BilibiliUserVO bilibiliUserVO = mapperFactory.getMapperFacade().map(retUser, BilibiliUserVO.class);
-            bilibiliFeignService.updateUser(bilibiliUserVO);
-
-            BilibiliUserContext.remove();
-            // 日志收集
-            String date = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.now());
-            File logFile = new File("logs/execution/all-" + date + ".log");
-            String content = null;
-            if (logFile.exists()) {
-                List<String> logs = FileUtil.readLines(logFile, StandardCharsets.UTF_8);
-
-                content = logs
-                        .stream()
-                        .filter(line -> line.contains(traceId) && (line.contains("INFO") || line.contains("ERROR")))
-                        .map(line -> line.split("\\|\\|")[1])
-                        .collect(Collectors.joining("\n"));
+        // 推送
+        try {
+            String traceId = future.get();
+            if (traceId != null) {
+                PUSH_QUEUE.put(taskConfig.getDedeuserid() + ":" + traceId);
             }
-            pushFeignService.push(dedeuserid, content);
-        });
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ret.get();
     }
 }
