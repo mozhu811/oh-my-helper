38af7eab62b0eee350932188b5e9f90aaf4a8d8a
cruii <cruii811@gmail.com>
2023-02-25 23:55:41 +0800

    :sparkles: 新增测试获取的代理

--- hh-common/src/main/java/io/cruii/util/ProxyUtil.java
+++ hh-common/src/main/java/io/cruii/util/ProxyUtil.java
@@ -1,10 +1,8 @@
 package io.cruii.util;
 
-import cn.hutool.core.io.IORuntimeException;
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
@@ -12,6 +10,10 @@ import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -25,7 +27,7 @@ import java.util.stream.Collectors;
 @Log4j2
 public class ProxyUtil {
     private static String proxyApi;
-    private static final List<String> proxyList = new ArrayList<>();
+    private static final List<Proxy> proxyList = new ArrayList<>();
 
     private static final ReentrantLock LOCK = new ReentrantLock();
 
@@ -43,51 +45,51 @@ public class ProxyUtil {
         }
     }
 
-    public static String get() {
+    public static Proxy get() {
         try {
             LOCK.lock();
             int size = proxyList.size();
             if (proxyList.isEmpty()) {
                 log.debug("获取代理地址");
-                String body = HttpRequest.get(proxyApi)
-                        .execute().body();
+                String body = HttpRequest.get(proxyApi).execute().body();
                 JSONObject resp = JSONUtil.parseObj(body);
                 JSONArray data = resp.getJSONArray("data");
                 size = data.size();
-                proxyList.addAll(data
-                        .stream()
+                proxyList.addAll(data.stream()
                         .map(JSONUtil::parseObj)
-                        .map(obj -> obj.getStr("ip") + ":" + obj.getInt("port"))
+                        .map(obj -> new Proxy(Proxy.Type.HTTP, new InetSocketAddress(obj.getStr("ip"), obj.getInt("port"))))
                         .collect(Collectors.toList()));
             }
-            String proxy = proxyList.get(RandomUtil.randomInt(size - 1));
+            Proxy proxy = proxyList.get(RandomUtil.randomInt(size - 1));
             log.debug("本次获取代理地址: {}", proxy);
             log.debug("当前剩余代理数: {}", proxyList.size());
 
-            return proxy;
+            if (testProxy(proxy)) {
+                return proxy;
+            } else {
+                log.debug("代理 {} 不可用，切换代理", proxy);
+                proxyList.remove(proxy);
+                return get();
+            }
         } finally {
             LOCK.unlock();
         }
-    }
 
-    @Deprecated
-    private static boolean checkProxy(String proxy) {
-        String host = proxy.split(":")[0];
-        int port = Integer.parseInt(proxy.split(":")[1]);
+    }
+    private static boolean testProxy(Proxy proxy) {
+        HttpURLConnection connection = null;
         try {
-            HttpResponse response = HttpRequest.get("https://www.bilibili.com")
-                    .setHttpProxy(host, port)
-                    .setConnectionTimeout(10000)
-                    .execute();
-            if (response.getStatus() == 200) {
-                log.debug("该代理地址可用");
-                return true;
-            }
-        } catch (IORuntimeException e) {
-            log.debug("该代理地址不可用");
+            connection = (HttpURLConnection) new URL("https://www.bilibili.com/").openConnection(proxy);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            int statusCode = connection.getResponseCode();
+            return statusCode == HttpURLConnection.HTTP_OK;
+        } catch (IOException e) {
             return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
-        log.debug("该代理地址不可用");
-        return false;
     }
 }
