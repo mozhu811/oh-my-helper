e24bf84d8650614e0799e89330a98fb782b6fa6e
cruii <cruii811@gmail.com>
2023-03-02 23:53:49 +0800

    :white_check_mark: some tests

--- hh-push/src/test/java/io/cruii/PushTest.java
+++ hh-push/src/test/java/io/cruii/PushTest.java
@@ -1,30 +1,18 @@
 package io.cruii;
 
-import cn.hutool.core.lang.Assert;
-import io.cruii.pojo.dto.PushMessageDTO;
-import io.cruii.push.service.PushService;
+import io.cruii.push.pusher.impl.TelegramBotPusher;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+
 @Log4j2
 public class PushTest {
 
-    @Autowired
-    private PushService pushService;
 
 
     @Test
-    public void testPush() {
-        PushMessageDTO pushMessageDTO = new PushMessageDTO();
-        pushMessageDTO.setDedeuserid("287969457");
-        pushMessageDTO.setContent("123test");
-        boolean res = pushService.push(pushMessageDTO);
-        Assert.isTrue(res);
+    public void testTelegram() {
+        TelegramBotPusher telegramBotPusher = new TelegramBotPusher("123", "123");
+        telegramBotPusher.push("123\n123");
     }
 }
